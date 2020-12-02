d219098d5996a6fdba909e502df19443776d6f8c
Justin Zhu <justin.zhu@sjsu.edu>
2020-12-01 23:54:23 -0800

    Final Debug session

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user.js');
 
 router.get('/getUsers', (req, res)=>{
   User.find({}).then(users => res.json(users));
